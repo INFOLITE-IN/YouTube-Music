c1f435acc6a2c0b143d82576448b5de027a3ba84
KAVIYARASAN (Official) <114557988+KAVIYARASAN-1997@users.noreply.github.com>
2023-04-11 14:25:57 +0530

    Update music.py

--- plugins/music.py
+++ plugins/music.py
@@ -100,13 +100,13 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
 
     # Cropping circle from thubnail
     image3 = image11.crop((280,0,1000,720))
-    #lum_img = Image.new('L', [720,720] , 0)
-   # draw = ImageDraw.Draw(lum_img)
-   # draw.pieslice([(0,0), (720,720)], 0, 360, fill = 255, outline = "white")
-   # img_arr =np.array(image3)
-    #lum_img_arr =np.array(lum_img)
-    #final_img_arr = np.dstack((img_arr,lum_img_arr))
-    #image3 = Image.fromarray(final_img_arr)
+    lum_img = Image.new('L', [720,720] , 0)
+   draw = ImageDraw.Draw(lum_img)
+    draw.pieslice([(0,0), (720,720)], 0, 360, fill = 255, outline = "white")
+   img_arr =np.array(image3)
+    lum_img_arr =np.array(lum_img)
+    final_img_arr = np.dstack((img_arr,lum_img_arr))
+    image3 = Image.fromarray(final_img_arr)
     image3 = image3.resize((500,500))
     
     image2.paste(image3, (100,115))
