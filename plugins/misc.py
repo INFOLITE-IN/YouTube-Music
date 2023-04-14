4ec404666f114b5afbce6335b7a2b9c9384b42eb
KAVIYARASAN (Official) <114557988+KAVIYARASAN-1997@users.noreply.github.com>
2023-04-14 10:17:00 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -125,7 +125,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"**• ᴜᴘʟᴏᴀᴅᴇᴅ ʙʏ​ » [ʏᴏᴜᴛᴜʙᴇ ᴍᴜsɪᴄ](t.me/{BOT_USERNAME}) 🍄\n• ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ​ » {chutiya}\n• sᴇᴀʀᴄʜᴇᴅ ғᴏʀ » {query}**"
+        rep = f"**• ᴜᴘʟᴏᴀᴅᴇᴅ ʙʏ​ » [ʏᴏᴜᴛᴜʙᴇ ᴍᴜsɪᴄ](t.me/{BOT_USERNAME}) 🍄\n• ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ​ » {0}\n• sᴇᴀʀᴄʜᴇᴅ ғᴏʀ » {query}**"
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
